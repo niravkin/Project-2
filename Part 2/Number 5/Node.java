95308b56f8e646a48cc76f0d65b5e58f0876d3f9
Nirav Kinkhabwala <niravkseeds@gmail.com>
2020-04-10 01:23:42 -0400

    Add files via upload

---
+++ Part 2/Number 5/Node.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Node {
+	public String val;
+	public LinkedHashMap<Node, Integer> neighbors;
+	public Node(final String nodeVal) {
+		val = nodeVal;
+		neighbors = new LinkedHashMap<Node, Integer>();
+	}
+}
